ddf929e7a165f294bcf74c727893589bf21d7ce0
Will Augustyn <waugustyn89@gmail.com>
2023-05-17 09:06:04 -0400

    add helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,18 +1,9 @@
-<<<<<<< HEAD
-def hello_world(state):
-    print(f'Hello World {state}!')
+def hello_world():
+    print(f'Hello World!')
 
 
 def main():
-    hello_world('New York')
-=======
-def hello_world(city):
-    print(f'Hello World [city]!')
-
-
-def main():
-    hello_world('NYC')
->>>>>>> 73ebc32c8ca49d52393e10cf26c02a515d2699ba
+    hello_world()
 
 
 main()
